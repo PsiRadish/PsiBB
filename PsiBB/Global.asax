0403535ad26c2f27212da411a9dc696fa68e0b4f
PsiRadish <psiradish@comcast.net>
2016-02-24 01:15:36 -0800

    Base ASP.NET MVC project.

---
+++ PsiBB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PsiBB.MvcApplication" Language="C#" %>
---
+++ PsiBB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PsiBB.Startup))]
+namespace PsiBB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
