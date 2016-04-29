4bee8a2a46f3073ccaa5806d6e1fa5b757473154
PsiRadish <psiradish@comcast.net>
2016-04-28 19:20:37 -0700

    Temporarily create Users from GET parameters

--- PsiBB/Controllers/HomeController.cs
+++ PsiBB/Controllers/HomeController.cs
@@ -16,11 +16,20 @@ namespace PsiBB.Controllers
         // GET: Home
         public async Task<ActionResult> IndexAsync()
         {
+            // temporarily create Users from GET parameters
+            var name = Request.QueryString["name"];
+            var email = Request.QueryString["email"];
+
+            if (name != null && email != null)
+            {
+                await (new Models.User { DisplayName = name, Email = email }).CreateAsync();
+                ViewBag.CreatedUserMessage = "Created user with Name " + name.ToString() + " and Email " + email.ToString();
+            }
+
             ViewBag.Message = "Congratulations!";
             // ViewBag.Message = CultureInfo.CurrentCulture.Name;
             
             ViewBag.UsersJSON = (await Models.User.GetAllAsync()).ToJson(new JsonWriterSettings { Indent = true });
-
             return View();
         }
     }
